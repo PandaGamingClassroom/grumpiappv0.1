4d763751d56d650c5fd054acc7c2f58e79038393
Dani G <gamificacionpanda@gmail.com>
2022-11-28 14:52:17 +0100

    Modify map

--- src/app/components/map/map.page.ts
+++ src/app/components/map/map.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
+import { Marcas } from '../../models/interfaces/geoLocation.interface';
 
 @Component({
   selector: 'app-map',
@@ -7,7 +8,7 @@ import * as mapboxgl from 'mapbox-gl';
   styleUrls: ['./map.page.scss'],
 
 })
-export class MapPage implements OnInit, AfterViewInit {
+export class MapPage implements OnInit, OnDestroy {
 
   /** Control del elemento HTML */
   @ViewChild('mapa') divMapa!: ElementRef;
@@ -16,19 +17,109 @@ export class MapPage implements OnInit, AfterViewInit {
   mapa!: mapboxgl.Map;
 
   /** Control del zoom en el mapa */
-  zoomLevel: number = 5;
+  zoomLevel: number = 5.18;
+
+  /**
+   * Listado de marcadores para el mapa
+   */
+  marcadores: mapboxgl.Marker[] = [];
+  listaMarcadores: Marcas[] = [];
+
+  /**
+   * 
+   * MARCADORES DE COMUNIDADES EN EL MAPA
+   * 
+   */
+  madridMarker: [number, number] = [-3.6968750107555226, 40.413968834548996];
+  granadaMarker: [number, number] = [-3.5921160031997386, 37.17662711972035];
+  almeriaMarker: [number, number] = [-2.457262929199076, 36.8465456517929];
+  malagaMarker: [number, number] = [-4.428196056164957, 36.73791493554032];
+  cadizMarker: [number, number] = [-6.189117854225306, 36.54827582580875];
+  sevillaMarker: [number, number] = [-5.981809821054786, 37.374947978046094];
+  huelvaMarker: [number, number] = [-6.941693596056062, 37.274399566670816];
+  cordobaMarker: [number, number] = [-4.791758753424231, 37.87378779802737];
+  jaenMarker: [number, number] = [-3.78598153268191, 37.7822072368544];
+
+
+
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    console.log();
+    
+   }
 
-  ngAfterViewInit(): void {
+
+  /**
+   * Parámetros para construir el mapa
+   */
+  buildMap() {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-3.6846281526065296, 40.41260465302904],
-      zoom: 5
+      style: 'mapbox://styles/mapbox/light-v11',
+      center: [-3.7365441697213613, 40.413732151960396],
+      zoom: this.zoomLevel
     });
+    this.mapa.on('zoom', (ev) => {
+      this.zoomLevel = this.mapa.getZoom();
+    });
+
+    this.mapa.on('zoomend', (ev) => {
+      if (this.mapa.getZoom() > 18) {
+        this.mapa.zoomTo(18);
+      }
+    });
+
+    this.markers();
+
+  }
+
+  ionViewDidEnter() {
+    this.buildMap();
+  }
+
+  /**
+   * Posición de las marcas en el mapa
+   */
+  markers() {
+    
+    this.marcadores = [
+      new mapboxgl.Marker()
+        .setLngLat(this.madridMarker)
+        .addTo(this.mapa),
+      new mapboxgl.Marker()
+        .setLngLat(this.granadaMarker)
+        .addTo(this.mapa),
+      new mapboxgl.Marker()
+        .setLngLat(this.almeriaMarker)
+        .addTo(this.mapa),
+      new mapboxgl.Marker()
+        .setLngLat(this.malagaMarker)
+        .setPopup(
+          new mapboxgl.Popup({ offset: 30 }) // add popups
+            .setHTML(
+              `<h3> Hola </h3><p> adios </p>`
+            )
+        )
+        .addTo(this.mapa),
+      new mapboxgl.Marker()
+        .setLngLat(this.cadizMarker)
+        .addTo(this.mapa),
+      new mapboxgl.Marker()
+        .setLngLat(this.sevillaMarker)
+        .addTo(this.mapa),
+      new mapboxgl.Marker()
+        .setLngLat(this.huelvaMarker)
+        .addTo(this.mapa),
+      new mapboxgl.Marker()
+        .setLngLat(this.cordobaMarker)
+        .addTo(this.mapa),
+      new mapboxgl.Marker()
+        .setLngLat(this.jaenMarker)
+        .addTo(this.mapa)
+    ];
+
   }
 
   /**
@@ -36,7 +127,6 @@ export class MapPage implements OnInit, AfterViewInit {
    */
   zoomOut() {
     this.mapa.zoomOut();
-    this.zoomLevel = this.mapa.getZoom();
   }
 
   /**
@@ -44,6 +134,35 @@ export class MapPage implements OnInit, AfterViewInit {
    */
   zoomIn() {
     this.mapa.zoomIn();
-    this.zoomLevel = this.mapa.getZoom();
+  }
+
+  /**
+   * 
+   * @param valor Recibe el valor del zoom de la barra
+   */
+  zoomCambio(valor: string) {
+    this.mapa.zoomTo(Number(valor));
+  }
+
+  irMarcador(marcador: mapboxgl.Marker) {
+    this.mapa.flyTo({
+      center: marcador.getLngLat(),
+      zoom: 9.36
+    });
+  }
+
+  agregarMarcador() {
+    const newMarker = new mapboxgl.Marker({
+      draggable: true
+    })
+      .setLngLat(this.madridMarker)
+      .addTo(this.mapa);
+
+    this.marcadores.push(newMarker);
+  }
+
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', () => { });
+    this.mapa.off('zoomend', () => { });
   }
 }
---
+++ src/app/models/interfaces/markers.interface.ts
@@ -0,0 +1,6 @@
+import * as mapboxgl from 'mapbox-gl';
+
+export interface Marcador {
+    color?: string;
+    marker?: mapboxgl.Marker;
+}
