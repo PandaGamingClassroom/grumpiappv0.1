a62600bf107f195afb925244244eafb45c89166e
Dani G <gamificacionpanda@gmail.com>
2022-11-29 12:19:44 +0100

    Add selection avatars

--- src/app/components/profile/profile.page.ts
+++ src/app/components/profile/profile.page.ts
@@ -14,9 +14,28 @@ export class ProfilePage implements OnInit {
   audio = new Audio('../../../assets/sound/button-click-off-click.mp3');
   isModalOpen = false;
 
+  /*********************
+   * Variables AVATAR
+   *********************/
+  modalAvatarsOpen = false;
+  avatar_list: any[] = [];
+  avatarSelect= '';
+
+  avatar0 = '../../../assets/img/avatar0.jpg';
+  avatar1 = '../../../assets/img/avatar1.png';
+  avatar2 = '../../../assets/img/avatar2.png';
+  avatar3 = '../../../assets/img/avatar3.png';
+  avatar4 = '../../../assets/img/avatar4.png';
+
   constructor(private modalCtrl: ModalController, private route: Router) { }
 
   ngOnInit() {
+    this.avatar_list[0] = this.avatar0;
+    this.avatar_list[1] = this.avatar1;
+    this.avatar_list[2] = this.avatar2;
+    this.avatar_list[3] = this.avatar3;
+    this.avatar_list[4] = this.avatar4;
+
   }
 
   clickSound() {
@@ -28,6 +47,10 @@ export class ProfilePage implements OnInit {
     this.isModalOpen = isOpen;
   }
 
+  openAvatars(isOpen: boolean) {
+    this.modalAvatarsOpen = isOpen;
+  }
+
   openInventory() {
     this.route.navigate(['/profile/inventory/medals']);
   }
@@ -35,4 +58,11 @@ export class ProfilePage implements OnInit {
   openCreatures() {
     this.route.navigate(['/profile/inventory/creatures']);
   }
+
+  avatarSelected(avatar: any) {
+    console.log('Avatar seleccionado: ', avatar);
+    this.avatarSelect = avatar;
+    this.modalAvatarsOpen = false;
+  }
+
 }
